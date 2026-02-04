c5b9db39c05ac398820acd9a4c99ee8ae1f43607
Igor Zagorodnyi <wmsangel@gmail.com>
2026-02-04 11:45:05 +0600

    Fixes

--- backend/config/database.js
+++ backend/config/database.js
@@ -25,11 +25,11 @@ function getConfig() {
         }
     }
     return {
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT, 10) || 3306,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        host: process.env.DB_HOST || process.env.MYSQLHOST,
+        port: parseInt(process.env.DB_PORT || process.env.MYSQLPORT, 10) || 3306,
+        user: process.env.DB_USER || process.env.MYSQLUSER,
+        password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+        database: process.env.DB_NAME || process.env.MYSQLDATABASE,
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
